85f8388bab816f2c8748795acfa5f96f3ac98522
Mmis1000 <mmis1000@yahoo.com.tw>
2014-09-03 18:09:11 +0800

    remove old file, update config example

--- server/config.exmaple.js
+++ server/config.exmaple.js
@@ -1,7 +1,11 @@
 var config = {
     port : 34689,
     publicFolder : "public",
-    index : "/index.htm"
+    index : "/index.htm",
+    
+    /*if you wish, set it to false to disable it*/
+    statsHTML : "/stats",
+    statsJSON : "/stats.json",
 };
 
 module.exports = config;
